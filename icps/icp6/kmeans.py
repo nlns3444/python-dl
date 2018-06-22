c40618888250f0466e01cc3a828a85d38d1dd461
nlns3444 <35547878+nlns3444@users.noreply.github.com>
2018-06-21 21:14:34 -0500

    Update kmeans.py

--- icps/icp6/kmeans.py
+++ icps/icp6/kmeans.py
@@ -11,7 +11,9 @@ kmeans = [KMeans(n_clusters=i) for i in Nc]# Additionally, we are also normalizi
 
 kmeans
 
-score = [kmeans[i].fit(Y).score(Y) for i in range(len(kmeans))]
+score = [kmeans[i].fit(Y).score(Y) for i in range(len(kmeans))]#score variable denotes the percentage of variance explained by the number of clusters.
+
+
 
 score
 
@@ -21,29 +23,31 @@ pl.xlabel('Number of Clusters')
 
 pl.ylabel('Score')
 
-pl.title('Elbow Curve')
+pl.title('Elbow Curve')#The elbow curve is then graphed using the pylab library.
 
 pl.show()# it shows all the plots we have mentioned abovr
 
 pca = PCA(n_components=1).fit(Y)
 
-pca_d = pca.transform(Y)
+pca_d = pca.transform(Y)# pca algorithm is being used to convert data that might be overly dispersed into a set of linear combinations that can more easily be interpreted.
+
+
 
 pca_c = pca.transform(X)
-kmeans=KMeans(n_clusters=3)
+kmeans=KMeans(n_clusters=3)#When we graph the plot, we see that the graph levels off rapidly after 3 clustersz
 
 kmeansoutput=kmeans.fit(Y)
 
 kmeansoutput
 
-pl.figure('3 Cluster K-Means')
+pl.figure('3 Cluster K-Means')#plots all the clusters
 
 pl.scatter(pca_c[:, 0], pca_d[:, 0], c=kmeansoutput.labels_)
 
-pl.xlabel('Dividend Yield')
+pl.xlabel('Dividend Yield')#plots the dividend yeild
 
-pl.ylabel('Returns')
+pl.ylabel('Returns')#plots the returns
 
 
 
-pl.show()
+pl.show()#shows all the plotted above things
